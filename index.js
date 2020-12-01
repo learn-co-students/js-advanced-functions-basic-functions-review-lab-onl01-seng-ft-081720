e6e3c23daf168da322249860c015879b297934bd
blen_meku <mekuriablen@gmail.com>
2020-12-01 01:45:22 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.` 
+}
+
+function wrapAdjective(string = "*"){
+    let innerFunction = function (word ="special") { 
+        return `You are ${string}${word}${string}!` 
+    }
+    return innerFunction
+}
+wrapAdjective("%")()
+
+let Calculator = {
+    add: function (x, y) {
+        return x + y
+    },
+    subtract: function (x, y) {
+        return x - y
+    },
+    multiply: function (x, y) {
+        return x * y
+    },
+    divide: function (x, y) {
+        return x / y
+    }
+}
+
+function actionApplyer(s, ray){
+    let a = s
+
+    for (let i = 0; i < ray.length; i++) {
+        a = ray[i](a)
+    }
+    return a
+}
